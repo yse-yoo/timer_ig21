e4dfdff5203546bc50b02f76dfe685df3655b89c
yse-yoo <youhei_yoshikawa@yse-c.net>
2022-06-10 11:05:35 +0900

    new project

---
+++ src/sample/SecondsTimer.java
@@ -0,0 +1,13 @@
+package sample;
+
+public class SecondsTimer {
+	
+	public static int seconds = 3;
+
+	public static void main(String[] args) {
+		
+		System.out.println(seconds + "秒後に実行します");
+
+	}
+
+}
