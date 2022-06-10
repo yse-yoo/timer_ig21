d05736c9fa5e6a1fb9a75be12fb685a65f1df014
yse-yoo <youhei_yoshikawa@yse-c.net>
2022-06-10 14:18:53 +0900

    add AnimationTask

--- src/school/AnimationTask.java
+++ src/school/AnimationTask.java
@@ -1,5 +1,23 @@
 package school;
 
-public class AnimationTask {
+import java.util.TimerTask;
+
+public class AnimationTask extends TimerTask {
+	
+	public Window window;
+	
+
+	public AnimationTask(Window window) {
+		super();
+		//Windowインスタンスの受け取り
+		this.window = window;
+	}
+
+
+	@Override
+	public void run() {
+		//タイマーが実行されるたびにアニメーション
+		window.animate();
+	}
 
 }
