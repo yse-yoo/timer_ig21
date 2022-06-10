d61267362d5402ed5a8bf08b85f3f2ad28142ca0
yse-yoo <youhei_yoshikawa@yse-c.net>
2022-06-10 11:12:47 +0900

    add SecondsTimer

--- src/sample/SecondsTimer.java
+++ src/sample/SecondsTimer.java
@@ -1,5 +1,7 @@
 package sample;
 
+import java.util.Timer;
+
 public class SecondsTimer {
 	
 	public static int seconds = 3;
@@ -7,7 +9,11 @@ public class SecondsTimer {
 	public static void main(String[] args) {
 		
 		System.out.println(seconds + "秒後に実行します");
-
+		//ミリセカンド(ms)に変換
+		long ms = seconds * 1000;
+		
+		Timer timer = new Timer();
+		
 	}
 
 }
